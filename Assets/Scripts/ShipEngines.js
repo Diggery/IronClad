7d03481de4b454989a78287ada69dff9266dcb5e
Collins Brian-KDX347 <kdx347@kdx347-mac00.(none)>
2012-05-27 08:26:59 -0700

    added a configurable joint version of the ship

--- Assets/Scripts/ShipEngines.js
+++ Assets/Scripts/ShipEngines.js
@@ -7,30 +7,38 @@ var manuveringPower 			: float;
 var enginePower					: HUDPower;
 var steeringWheel				: Transform;
 
+var joint						: ConfigurableJoint;
+
 
 function Start () {
 
 }
 
 function FixedUpdate () {
-	//point ship to match the steering wheel
-	heading = steeringWheel.eulerAngles.y;
-	if (heading > 180) heading = -360 + heading;
-	
-	currentAngle = transform.localEulerAngles.y;
-	if (currentAngle > 180) currentAngle = -360 + currentAngle;
-	
-	var turnLeft : boolean;
-	
-	if (currentAngle - heading > 0) turnLeft = true;
-	
-	if (turnLeft) {
-		rigidbody.AddTorque(0, -manuveringPower, 0);
+	if (!joint) {
+		//point ship to match the steering wheel
+		heading = steeringWheel.eulerAngles.y;
+		if (heading > 180) heading = -360 + heading;
+		
+		currentAngle = transform.localEulerAngles.y;
+		if (currentAngle > 180) currentAngle = -360 + currentAngle;
+		
+		var turnLeft : boolean;
+		
+		if (currentAngle - heading > 0) turnLeft = true;
+		
+		if (Mathf.Abs(currentAngle - heading) > 180 ) turnLeft = !turnLeft;
+		
+		if (turnLeft) {
+			rigidbody.AddTorque(0, -manuveringPower, 0);
+		} else {
+			rigidbody.AddTorque(0, manuveringPower, 0);
+		}
+		
+		//set the power of the ship
+		
+		rigidbody.AddRelativeForce(0, 0, 10000 * enginePower.getPowerLevel());
 	} else {
-		rigidbody.AddTorque(0, manuveringPower, 0);
-	}
-	
-	//set the power of the ship
-	
-	rigidbody.AddRelativeForce(0, 0, 10000 * enginePower.getPowerLevel());
+		joint.targetRotation = steeringWheel.localRotation;
+	}	
 }
